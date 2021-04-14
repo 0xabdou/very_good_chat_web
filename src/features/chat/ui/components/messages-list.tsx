1620a404cf57bf65116a8f22d63394d23794a823
aouahib <ouahibdou@gmail.com>
2021-04-14 18:05:20 +0000

    Fix scroll bug. Add arrow button to scroll to bottom

--- src/features/chat/ui/components/messages-list.tsx
+++ src/features/chat/ui/components/messages-list.tsx
@@ -1,13 +1,14 @@
-import React, {useCallback, useState} from "react";
-import {makeStyles} from "@material-ui/core";
+import React, {useCallback, useEffect, useRef, useState} from "react";
+import {Icon, IconButton, makeStyles} from "@material-ui/core";
 import Message from "../../types/message";
 import MessageListItem from "./message-list-item/message-list-item";
-import {ItemContent, Virtuoso} from "react-virtuoso";
+import {ItemContent, ListItem, Virtuoso, VirtuosoHandle} from "react-virtuoso";
 import AutoSizer from "react-virtualized-auto-sizer";
 import Conversation from "../../types/conversation";
 import {useAppDispatch} from "../../../../core/redux/hooks";
 import useChatActions from "../../chat-actions-provider";
 import {PulseLoader} from "react-spinners";
+import {Theme} from "@material-ui/core/styles/createMuiTheme";
 
 export type MessagesListProps = {
   conversation: Conversation,
@@ -20,7 +21,16 @@ const MessagesList = (props: MessagesListProps) => {
   const [initialTopMostItemIndex] = useState(props.conversation.messages.length - 1);
   const dispatch = useAppDispatch();
   const {getMoreMessages} = useChatActions();
-  const classes = useStyles();
+  const ref = useRef<VirtuosoHandle>(null);
+  const isAtBottom = useRef<boolean | null>(null);
+  const lastMsgID = useRef<number | null>(null);
+  const [showArrow, setShowArrow] = useState(false);
+  const classes = useStyles({showArrow});
+
+  useEffect(() => {
+    lastMsgID.current =
+      props.conversation.messages[props.conversation.messages.length - 1].id;
+  }, [props.conversation.messages]);
 
   const itemContent: ItemContent<Message> = useCallback((index) => {
     return (
@@ -43,6 +53,40 @@ const MessagesList = (props: MessagesListProps) => {
     }
   }, [props.conversation, setFirstItemIndex, dispatch]);
 
+  const scrollToBottom = useCallback(() => {
+    ref.current?.scrollToIndex({
+      index: START_INDEX + 1000,
+      behavior: "smooth"
+    });
+  }, [ref.current]);
+
+  const bottomStateChanged = useCallback((state: boolean) => {
+    isAtBottom.current = state;
+  }, [isAtBottom]);
+
+  const followOutput = useCallback(
+    () => {
+      const lastMsg = props.conversation.messages[props.conversation.messages.length - 1];
+      if (lastMsg.id != lastMsgID.current) {
+        if (isAtBottom.current || lastMsg.senderID == props.currentUserID) {
+          setTimeout(scrollToBottom, 100);
+        }
+      }
+      return false;
+    },
+    [props.conversation.messages,
+      props.currentUserID,
+      isAtBottom.current,
+      lastMsgID.current,
+      ref.current]
+  );
+
+  const itemsRendered = useCallback((items: ListItem<Message>[]) => {
+    const last = items[items.length - 1]?.data?.id;
+    if (last != lastMsgID.current) setShowArrow(true);
+    else setShowArrow(false);
+  }, [lastMsgID.current, setShowArrow]);
+
   return (
     <div className={classes.outer}>
       <AutoSizer>
@@ -55,21 +99,27 @@ const MessagesList = (props: MessagesListProps) => {
               startReached={getMore}
               itemContent={itemContent}
               style={{height, width, overflowX: 'hidden'}}
-              followOutput="smooth"
+              atBottomStateChange={bottomStateChanged}
+              followOutput={followOutput}
               overscan={600}
               components={{
                 Header: () => <ListLoader
                   loading={props.conversation.fetchingMore}/>
               }}
+              ref={ref}
+              itemsRendered={itemsRendered}
             />
           );
         }}
       </AutoSizer>
+      <IconButton className={classes.arrow} onClick={scrollToBottom}>
+        <Icon className={classes.arrowIcon}>arrow_downward</Icon>
+      </IconButton>
     </div>
   );
 };
 const ListLoader = ({loading}: { loading?: boolean }) => {
-  const classes = useStyles();
+  const classes = useStyles({});
   if (!loading) return <div/>;
   return (
     <div className={classes.loader}>
@@ -78,17 +128,33 @@ const ListLoader = ({loading}: { loading?: boolean }) => {
   );
 };
 
-const useStyles = makeStyles({
+const useStyles = makeStyles<Theme, { showArrow?: boolean }>({
   outer: {
+    position: "relative",
     width: '100%',
     flexGrow: 1,
-    overflowY: 'auto',
+    overflow: "hidden",
   },
   loader: {
     display: 'flex',
     justifyContent: "center",
     alignItems: "center",
     padding: '16px',
+  },
+  arrow: props => ({
+    position: "absolute",
+    right: 0,
+    left: 0,
+    margin: "0 auto",
+    bottom: props.showArrow ? 8 : -50,
+    transition: '500ms',
+    background: "black",
+    "&:hover": {
+      background: "rgba(0, 0, 0, 0.7)",
+    }
+  }),
+  arrowIcon: {
+    color: 'white',
   }
 });
 
